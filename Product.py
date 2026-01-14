0ddbe5617bf96c5f24eecfed23d1d9fbb91ed469
wakibd-glitch <wakibd@gmail.com>
2026-01-14 11:54:38 +1100

    Add files via upload

---
+++ Product.py
@@ -0,0 +1,19 @@
+import math
+
+product1 = 1
+product2 = 1
+tupe1 = (4,3,2,2,-1,18)
+tupe2 = (2, 4, 8, 8, 3 ,2 ,9)
+
+for x in tupe1:
+    product1 *= x
+
+print("tupe1: " , product1)
+
+
+for y in tupe2:
+    product2 *= y
+
+print("tupe2: " , product2)
+
+
