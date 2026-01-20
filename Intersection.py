4c1505e1ceb20e7d26ed3e40575173719d466be5
wakibd-glitch <wakibd@gmail.com>
2026-01-20 16:01:59 +1100

    Add files via upload

---
+++ Intersection.py
@@ -0,0 +1,9 @@
+setx = {1,2,3,4}
+sety = {3,4,5,6}
+
+print("Original set elements:")
+print(setx)
+print(sety)
+print("\nIntersection of two said sets:")
+setz = setx.intersection(sety)
+print(setz)
