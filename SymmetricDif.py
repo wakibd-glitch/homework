2a90a87e765fa109b739ae726b46812346ccc92f
wakibd-glitch <wakibd@gmail.com>
2026-01-22 11:12:14 +1100

    Add files via upload

---
+++ SymmetricDif.py
@@ -0,0 +1,17 @@
+print("")
+
+Set1 = {'blue','green'}
+Set2 = {'blue', 'yellow'}
+Set3 = Set1.symmetric_difference(Set2)
+print("Set Difference A : " , Set3)
+# First set differenced A
+
+print("")
+
+Set4 = {1,2,3,4,5}
+Set5 = {1,5,6,7,8,9}
+Set6 = Set4.symmetric_difference(Set5)
+print("Set Difference B : " , Set6)
+# Second set differenced B
+
+print("")
